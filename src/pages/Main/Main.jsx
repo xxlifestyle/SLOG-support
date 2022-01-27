42faed4f7213f1b92532baea1c55f39aa38bb909
Vladislav <32987720+xxlifestyle@users.noreply.github.com>
2022-01-27 15:20:07 +0300

    Добавлены иконки подписки

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -6,7 +6,8 @@ import {useNavigate} from "react-router-dom";
 import user from "../../user.jpg"
 import Messenger from "./Messenger/Messenger";
 import logo from "../../icon.png"
-
+import Bell from "../../bell.svg"
+import BellSlash from "../../bell-slash.svg"
 
 const Main = () => {
     const [chatData, setChatData] = useState([])
@@ -45,11 +46,22 @@ const Main = () => {
                  }
              </div>
         <div className={'text-form'}>
-            <p className={'name-form'}>{data.name}</p>
+            <div className={'name-form'}>{data.name}</div>
+
+            <div className={'subscribe-btn'}>
+                { data.is_active == false &&
+            <div className={"bell"}><img src={Bell} alt=""/></div>
+                }
+                {data.is_active == true &&
+                    <div className={"bell-slash"}><img src={BellSlash} alt=""/></div>
+                }
+            </div>
+            <div className={'unread'} >
             {data.unread_count != 0 &&
-            <div className={'undread-block'}>
+            <div className={'undread-block'} >
                 {data.unread_count}
             </div>}
+            </div>
         </div>
 
     </div>)}
